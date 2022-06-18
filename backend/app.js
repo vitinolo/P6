34670d9b8491b347b0b3287396ad5257eb46a9a2
thibaut lenoir <lenoir677@gmail.com>
2022-06-18 17:40:01 +0200

    ajout des fichiers pour authentification utlisateur

---
+++ backend/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const userRoutes = require('./routes/user');
+
+mongoose.connect('mongodb+srv://new-user72:Créet1base2donné@cluster0.j4qcu8e.mongodb.net/?retryWrites=true&w=majority',
+{ useNewUrlParser: true,
+  useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+  
+app.use((req, res, next) => {
+res.setHeader('Access-Control-Allow-Origin', '*');
+res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+next();
+});
+
+app.use(express.json());
+app.use('/api/auth', userRoutes);
+
+module.exports = app;
